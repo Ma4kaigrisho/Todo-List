c7ebff75b62f3151f82a8444a1e57bdd234e6617
Ma4kaigrisho <ivanovnikola0021@gmail.com>
2023-07-30 19:14:46 +0300

    list creation done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-$("p").hide("fast");
-$("p").toggle("slow",function () {
-    $(this).addClass("blue");  
-})
-$("button").on("click",function(){
-    $("p").stop();
+$(document).ready(function(){
+    $(".btn").click(function(textval){
+        textval = $("#task").val();
+        $("#tasks").append(`<li class = "task">`+ textval +` <i class="bx bxs-edit bx-sm"></i> <i class="bx bxs-trash bx-sm"></i></li>`);
+    })
+    
 })
