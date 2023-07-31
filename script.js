313690f05e82874dd10bf05621131cbcd22f26a8
Ma4kaigrisho <ivanovnikola0021@gmail.com>
2023-07-31 08:40:11 +0300

    remove

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
     $(".btn").click(function(textval){
         textval = $("#task").val();
-        $("#tasks").append(`<li class = "task">`+ textval +` <i class="bx bxs-edit bx-sm"></i> <i class="bx bxs-trash bx-sm"></i></li>`);
+        $("#tasks").append(`<li class = "task"> <p>`+ textval +` </p><i class="bx bxs-edit bx-sm"></i> <i class="bx bxs-trash bx-sm"></i></li>`);
     })
-    
+    $(".bxs-edit").click(function(){
+        console.log(this.previousElementSibling)
+    })
+    $("#tasks").on("click", ".bxs-trash" , function(){
+        $(this).parent().remove();
+    })
 })
