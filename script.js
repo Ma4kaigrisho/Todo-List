5943659203e3f6dcd77f56a667ecace991440349
Ma4kaigrisho <ivanovnikola0021@gmail.com>
2023-07-31 16:48:10 +0300

    cancel button

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 $(document).ready(function(){
-    $(".btn").click(function(textval){
+    $("#submit-btn").click(function(textval){
         textval = $("#task").val();
         $("#tasks").append(`<li class = "task"> <p>`+ textval +` </p><i class="bx bxs-edit bx-sm"></i> <i class="bx bxs-trash bx-sm"></i></li>`);
-    })
-    $(".bxs-edit").click(function(){
-        console.log(this.previousElementSibling)
     })
     $("#tasks").on("click", ".bxs-trash" , function(){
         $(this).parent().remove();
     })
+    function setDefault(){
+        $("#task").val("");
+        $("#submit-btn").text("Submit");
+        $("#submit-btn").removeClass("edit")
+        $(".cancel-btn").addClass("hide");
+        $(".bx").show();
+    }
+    $("#tasks").on("click" , ".bxs-edit", function(){
+        console.log($(this).prev("p").html());
+        $("#task").val($(this).prev("p").html());
+        $("#submit-btn").text("Edit");
+        $("#submit-btn").addClass("edit")
+        $(".cancel-btn").removeClass("hide");
+        $(".bx").hide();
+    })
+    $(".list").on("click",".edit", function(){
+
+    })
+    
 })
