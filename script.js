6c5cf5ec4bed01b27c88ff00bc0f6b0fa158b2e1
Ma4kaigrisho <ivanovnikola0021@gmail.com>
2023-07-31 23:07:43 +0300

    functionality ready

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
+    let editIndex = -1;
     $("#submit-btn").click(function(textval){
         textval = $("#task").val();
-        $("#tasks").append(`<li class = "task"> <p>`+ textval +` </p><i class="bx bxs-edit bx-sm"></i> <i class="bx bxs-trash bx-sm"></i></li>`);
+        if(textval.trim() != ""){
+            if(editIndex === -1){
+                $("#tasks").append(`<li class = "task"> <p>`+ textval +` </p><i class="bx bxs-edit bx-sm"></i> <i class="bx bxs-trash bx-sm"></i></li>`);
+            }
+            else{
+                $("#tasks li").eq(editIndex).find("p").text(textval);
+                setDefault();
+            }
+        }
+        
     })
     $("#tasks").on("click", ".bxs-trash" , function(){
         $(this).parent().remove();
@@ -14,14 +24,15 @@ $(document).ready(function(){
         $(".bx").show();
     }
     $("#tasks").on("click" , ".bxs-edit", function(){
-        console.log($(this).prev("p").html());
+        editIndex = $(this).parent().index();
         $("#task").val($(this).prev("p").html());
         $("#submit-btn").text("Edit");
         $("#submit-btn").addClass("edit")
         $(".cancel-btn").removeClass("hide");
         $(".bx").hide();
     })
-    $(".list").on("click",".edit", function(){
+    $(".cancel-btn").on("click", function(){
+        setDefault();
 
     })
     
